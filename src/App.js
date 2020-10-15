45b14583b8519f230a56eaa77313d3a7e1e08a82
misakikondo <misakikondo718@gmail.com>
2020-10-15 20:00:51 +0900

    ルートの設定
    ルートにParams idを付けて、そのページが表示されるようにする

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const App = () => (
   <div>
     <Route exact path="/" component={Home}/>
     <Route path="/about" component={About}/>
+    <Route path="/blog/:id" component={Blog}/>
     </div>
     </BrowserRouter>
 )
@@ -18,15 +19,28 @@ const Home = () => {
   return (
     <div class="container text-center mt-5">
       <h1>Welcome</h1>
+
+      <p><Link to="/about">About</Link></p>
+      //aboutに行けるリンク
     </div>
   )
 }
 
 const About = () => {
-  //Aboutページの中身
   return (
     <div class="container text-center mt-5">
       <h1>About</h1>
+    </div>
+  )
+}
+//Aboutページの中身
+
+const Blog = props => {
+  const { id } = props.match.params
+
+  return (
+    <div>
+      <p>{ id }番目の記事です</p>
     </div>
   )
 }
